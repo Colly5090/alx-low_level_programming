7486edbe83b055b4ba89193aca497563d7897944
Collins Boakye <colly0087@gmail.com>
2023-06-15 15:23:44 -0700

    Added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - An entry program path
- * Return: 1 (Success)
+ * main - A program entry path
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n", stdout);
 return (1);
 }
