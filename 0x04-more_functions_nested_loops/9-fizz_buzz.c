6dd263a7caef51049426f50e29938a3ed3ef34e3
Collins Boakye <colly0087@gmail.com>
2023-06-22 14:08:44 -0700

    fuzzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,16 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
 		if (x % 15 == 0)
 			printf("FizzBuzz");
 		else if (x % 3 == 0)
 			printf("Fizz");
 		else if (x % 5 == 0)
-			printf("Bizz");
+			printf("Buzz");
 		else
-			printf("%d", i);
+			printf("%x", x);
 		if (x < 100)
 			printf(" ");
 	}
