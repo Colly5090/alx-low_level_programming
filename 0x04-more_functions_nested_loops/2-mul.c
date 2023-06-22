88bdb3e3568199568433c7325336b1d658381987
Collins Boakye <colly0087@gmail.com>
2023-06-22 13:45:09 -0700

    2-mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * mul - an entry point of  two integers
+  * @a: - first integer
+  * @b: - second integer
+  * Return: 0 success
+  */
+
 int mul(int a, int b)
 {
-	return a * b;
+int result = a * b;
+return (result);
 }
