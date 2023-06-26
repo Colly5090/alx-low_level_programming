5292058d881075278f751970603b6b4278cf53fb
Collins Boakye <colly0087@gmail.com>
2023-06-26 13:38:00 -0700

    modified

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,32 @@
 #include "main.h"
 /**
-  * puts_half - The half last part of numbers
-  * @str: The half numbers to check
+  * puts_half - entry point to the function
+  * @str: half string to print
   * Return: void
   */
 void puts_half(char *str)
 {
-	int full_numb, half_numb;
+	int full_numb, x, n;
 
 	full_numb = 0;
+
 	while (str[full_numb] != '\0')
-		full_numb++;
-
-	half_numb = full_numb / 2;
-
-	if (full_numb % 2 == 1)
-		full_numb++;
-	while (half_numb < full_numb)
 	{
-		_putchar(str[half_numb]);
-		half_numb++;
+		full_numb++;
+	}
+
+	if (full_numb % 2 == 0)
+	{
+		for (n = full_numb / 2; str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	} else if (full_numb % 2)
+	{
+		for (x = (full_numb - 1) / 2; x < full_numb - 1; x++)
+		{
+			_putchar(str[x + 1]);
+		}
 	}
 	_putchar('\n');
 }
-
