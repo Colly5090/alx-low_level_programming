7451d2a01f794ddf5b5c0056c7b87451949530ed
Collins Boakye <colly0087@gmail.com>
2023-06-26 13:18:52 -0700

    modified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * reset_to_98 - The number same as the pointer.
-  * @n - number to be checked
+  * @n: number to be checked
   * Return: void
   */
 void reset_to_98(int *n)
