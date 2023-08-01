b7eaea16a28c5a037368c57561ff8fef78c3fdf8
Collins Boakye <colly0087@gmail.com>
2023-08-01 07:10:27 -0700

    reverse nodes

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,7 +13,7 @@ listint_t *reverse_listint(listint_t **head)
 	while (currentNode != NULL)
 	{
 		*head = currentNode->next;
-		curentNode->next = endNode;
+		currentNode->next = endNode;
 		endNode = currentNode;
 		currentNode = *head;
 	}
