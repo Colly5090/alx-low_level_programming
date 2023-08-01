3fc4a357862fc39b2595c99a87772788bc11bbd6
Collins Boakye <colly0087@gmail.com>
2023-07-31 19:18:31 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - To print all elements of the node list_t
+ * @h: all type of list_t to print
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int number = 0;
+	const listint_t *searcher;
+
+	searcher = h;
+	while (searcher)
+	{
+		number++;
+		searcher = searcher->next;
+	}
+	return (number);
+}
