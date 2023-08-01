94a7ad79fad393a012b0ad94643150077dc4c391
Collins Boakye <colly0087@gmail.com>
2023-08-01 08:34:11 -0700

    free safely

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - to free list of all nodes
+ * @h: pointer to first node to start freeing
+ * Return: elements to be freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *searcher;
+	size_t length;
+	unsigned int i;
+
+	if (h == NULL || *h->next == NULL)
+		return (0);
+
+	while (*h)
+	{
+		i = *h - (*h)->next;
+		if (i > 0)
+		{
+			searcher = (*h)->next;
+			free(*h);
+			*h = searcher;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (length);
+}
