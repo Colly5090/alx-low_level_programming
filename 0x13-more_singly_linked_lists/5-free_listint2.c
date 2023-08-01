bf9d1ee1c538941ae8505ce098fa3afc516a7950
Collins Boakye <colly0087@gmail.com>
2023-07-31 21:22:38 -0700

    more freed

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - to free lits of nodes
+ * @head: pointer to start of nodes to be freed
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *searcher;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		searcher = (*head)->next;
+		free(*head);
+		searcher = *head;
+	}
+}
