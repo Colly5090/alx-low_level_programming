43a0d266c2ac1487a2d67dc0aa736df77de32697
Collins Boakye <colly0087@gmail.com>
2023-08-03 11:03:24 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - to flip the bits
+ * @n: input number to be printed in bits
+ * @m: current bit to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flipBit = n ^ m;
+	unsigned int counter = 0;
+
+	while (flipBit)
+	{
+		flipBit &= (flipBit - 1);
+		counter++;
+	}
+	return (counter);
+}
