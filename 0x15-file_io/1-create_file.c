603fe28ca650a642480e6d4c7a698f1ec09887b0
Collins Boakye <colly0087@gmail.com>
2023-08-07 08:25:56 -0700

    edited

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int c;
+	int c = 0;
 	int myW;
 
 	if (filename == NULL)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int c;
+	int c = 0;
 	int myW;
 
 	if (filename == NULL)
