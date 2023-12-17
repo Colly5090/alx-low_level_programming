cf7a7554a2379edc8badad12a617db4525564d7d
Collins Boakye <colly0087@gmail.com>
2023-12-17 05:00:55 -0800

    file I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - a function to create a file
+ * @filename: the file to be created
+ * @text_content: A NULL terminated string
+ * Return: 1 Success -1 failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int c = 0;
+	int myW;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (c = 0; text_content[c];)
+			c++;
+	}
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	myW = write(fd, text_content, c);
+
+	if (fd == -1 || myW == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
